706fc9987f8f47df00cf8924b8141ce6bade5661
Stephanie Natasya <tasya315@gmail.com>
2024-05-06 14:27:22 +0700

    update routes index

--- index.js
+++ index.js
@@ -11,9 +11,13 @@ app.use(require("body-parser").json());
 
 const farmRouter = require("./src/routes/farmRoutes");
 const barnRouter = require("./src/routes/barnRoutes");
+const chefRouter = require("./src/routes/chefRoutes");
+const sellerRouter = require("./src/routes/sellerRoutes");
 
 app.use("/api/v1/farm", farmRouter);
 app.use("/api/v1/barn", barnRouter);
+app.use("/api/v1/chef", chefRouter);
+app.use("/api/v1/seller", sellerRouter);
 
 app.get("/", (req, res) => res.send("Hello World!"));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
