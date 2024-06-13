d13b9513b084ae732a175df83145f7333678e9a2
NubStupid <ferdinandsh04@gmail.com>
2024-06-13 20:49:43 +0700

    fixing verifyRole middleware

--- src/middlewares/verifyRole.js
+++ src/middlewares/verifyRole.js
@@ -1,17 +1,19 @@
 const verifyRole = (...allowedRoles) =>{
     return (req,res,next) => {
         let roles = req.roles
-        roles = (roles.toString().includes(",")? roles.split(","):roles)
         let proceed = false;
-        if(Array.isArray(roles)){
-            for (const role of roles) {
-                if(allowedRoles.includes(role)){
+        if(roles != null){
+            roles = (roles.toString().includes(",")? roles.split(","):roles)
+            if(Array.isArray(roles)){
+                for (const role of roles) {
+                    if(allowedRoles.includes(role)){
+                        proceed = true
+                    }
+                }
+            }else{
+                if(allowedRoles.includes(roles)){
                     proceed = true
                 }
-            }
-        }else{
-            if(allowedRoles.includes(roles)){
-                proceed = true
             }
         }
         if(proceed == false){
