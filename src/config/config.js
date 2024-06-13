7cd8cdfaae1dd2c777e1a6e12d08de3625348693
Stephanie Natasya <tasya315@gmail.com>
2024-06-13 15:33:38 +0700

    update config

--- src/config/config.js
+++ src/config/config.js
@@ -7,28 +7,6 @@ module.exports = {
         password: process.env.DB_PASS,
         port: process.env.DB_PORT,
         database: process.env.DB_DBNAME,
-        dialect: "mysql",
-    },
-    development: {
-        username: process.env.DB_USER,
-        password: process.env.DB_PASS,
-        database: process.env.DB_DBNAME,
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
         dialect: process.env.DB_DIALECT,
-    },
-    test: {
-        username: "root",
-        password: null,
-        database: "database_test",
-        host: "127.0.0.1",
-        dialect: "mysql",
-    },
-    production: {
-        username: "root",
-        password: null,
-        database: "database_production",
-        host: "127.0.0.1",
-        dialect: "mysql",
-    },
+    }
 };
