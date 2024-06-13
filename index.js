da322ff41e76fffaeb95e03bf482f2e370806d50
Albert Nathanael Hidayat <128572415+Diversi0n@users.noreply.github.com>
2024-06-13 13:09:06 +0700

    Tambah Koneksi Mongoose

--- index.js
+++ index.js
@@ -1,13 +1,19 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const app = express();
+const express = require('express')
+const cookieParser = require('cookie-parser');
+const app = express()
 const port = 3000;
+const mongoose = require("mongoose");
+const cors = require("cors");
 
 require("dotenv").config();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 app.use(require("body-parser").json());
+app.use(cors({
+    origin: "http://localhost:5173",
+    optionsSuccessStatus: 200
+}));
 
 
 const adminRouter = require("./src/routes/adminRoutes")
@@ -27,4 +33,6 @@ app.use("/api/v1/user",[AuditLog], userRouter);
 
 
 app.get("/", (req, res) => res.send("Hello World!"));
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+mongoose.connect("mongodb://127.0.0.1/db_vct").then(function () {
+    app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+});
